bdc5f358e3459be2cbebed12c6c93989f0adc973
khushboo-590 <khushbook4979@gmail.com>
2025-05-01 16:56:53 +0530

    images change

--- src/components/Data.js
+++ src/components/Data.js
@@ -2,17 +2,11 @@ export const profile = [
     {
         name: "Khushboo",
         post: "Frontend Developer",
-        image: "src/assets/images/png/profile-img.jpg",
+        image: "ssrc/assets/images/png/img4.png",
         email: "khushboo@gmail.com",
         location: "Hariyana",
     },
-    {
-        name: "Khushboo",
-        post: "Frontend Developer",
-        image: "src/assets/images/png/profile-img.jpg",
-        email: "khushboo@gmail.com",
-        location: "Hariyana",
-    },
+   
 ];
 
 export default profile;
